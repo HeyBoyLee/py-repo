d3962f09420d5754ae6d537eb6bd5b88cb67727a
lihuifeng1 <lihuifeng1@xiaomi.com>
2016-09-27 14:35:37 +0800

    add wsgi & flask demo

---
+++ wsgi/hello.py
@@ -0,0 +1,6 @@
+def application(environ, start_response):
+    method = environ['REQUEST_METHOD']
+    path = environ['PATH_INFO']
+    print 'method: %s, path: %s' % (method, path)
+    start_response('200 OK', [('Content-Type', 'text-html')])
+    return [b'<h1>Hello, web!</h1>']
