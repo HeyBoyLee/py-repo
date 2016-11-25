09f332d08e39c695cbefc73f64e98866fe522a8f
lihuifeng1 <lihuifeng1@xiaomi.com>
2016-11-25 19:16:07 +0800

    add object test

---
+++ object/object_test.py
@@ -0,0 +1,75 @@
+#! /usr/bin/python
+# encoding : utf-8
+
+class A(object):
+  def go(self):
+    print "go A go!"
+
+  def stop(self):
+    print "stop A stop"
+
+  def pause(self):
+    print "pause A pause"
+
+
+class B(A):
+  def go(self):
+    super(B, self).go()
+    print "go B go"
+
+class C(A):
+  def go(self):
+    super(C, self).go()
+    print "go C go"
+
+  def stop(self):
+    super(C, self).stop()
+    print "stop C stop"
+
+class D(B, C):
+  def go(self):
+    super(D, self).go()
+    print "go D go"
+
+  def stop(self):
+    super(D, self).stop()
+    print "stop D stop"
+
+  def pasue(self):
+    super(D, self).pause()
+    print "pasue D pause"
+
+
+class E(B, C):
+  pass
+
+
+a = A()
+b = B()
+c = C()
+d = D()
+e = E()
+
+# a.go()
+# b.go()
+# c.go()
+d.go()
+# e.go()
+#
+# a.stop()
+# b.stop()
+# c.stop()
+d.stop()
+# e.stop()
+#
+# a.pause()
+# b.pause()
+# c.pause()
+d.pause()
+# e.pause()
+
+
+
+
+
+
